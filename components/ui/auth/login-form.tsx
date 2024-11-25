d18f32ab71165debfd5ac3fe95f6c13521a7d14d
AyushRay06 <rayayush47@gmail.com>
2024-11-26 01:51:27 +0530

    loginForm

--- components/ui/auth/login-form.tsx
+++ components/ui/auth/login-form.tsx
@@ -1,6 +1,29 @@
+"use client"
+
 import CardWrapper from "./card-wrapper"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../form"
+import { LoginSchema } from "@/schemas"
+import { Input } from "../input"
 
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
   return (
     <CardWrapper
       headerLabel="Welcome Back"
@@ -8,7 +31,42 @@ const LoginForm = () => {
       backButtonLabel="Dont't have am account?"
       showSocials
     >
-      Login Form
+      <Form {...form}>
+        <form onSubmit={() => {}} className="space-y-6">
+          <div className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="rayayush47@gmail.com"
+                      {...field}
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="********" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   )
 }
